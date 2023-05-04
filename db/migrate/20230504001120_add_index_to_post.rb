9ba9516892237fa0891d9f042d6b2f8e8e7c2434
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-04 02:24:35 +0200

    Fixing linters errors

--- db/migrate/20230504001120_add_index_to_post.rb
+++ db/migrate/20230504001120_add_index_to_post.rb
@@ -1,5 +1,5 @@
 class AddIndexToPost < ActiveRecord::Migration[7.0]
   def change
-    add_index :posts, :author_id, name: "index_posts_on_author_id"
+    add_index :posts, :author_id, name: 'index_posts_on_author_id'
   end
 end
--- db/migrate/20230504001426_add_index_to_comment.rb
+++ db/migrate/20230504001426_add_index_to_comment.rb
@@ -1,6 +1,6 @@
 class AddIndexToComment < ActiveRecord::Migration[7.0]
   def change
-    add_index :comments, :author_id, name: "index_comments_on_author_id"
-    add_index :comments, :post_id, name: "index_comments_on_post_id"
+    add_index :comments, :author_id, name: 'index_comments_on_author_id'
+    add_index :comments, :post_id, name: 'index_comments_on_post_id'
   end
 end
