9d78dea0d1753d7d45879366dba6a0efa946cc96
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-04 16:12:16 +0200

    Removing comment in user.rb file toward Reviewer directionsn

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
 
   def recent_post
-    # posts.order (:created_at :desc).limit(3)
     posts.order(created_at: :desc).limit(3)
   end
 end
