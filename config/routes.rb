df791932104dcfa6e6ff1105ea9f57ba73d09fad
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-09 18:42:28 +0200

    Add controllers, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :likes
   resources :posts
   resources :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
