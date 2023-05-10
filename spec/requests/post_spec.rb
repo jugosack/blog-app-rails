7530e7b7c30bfd1521fffc7382f8abcb8ee86124
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-10 23:59:24 +0200

    Add test for testing response status in /show

--- spec/requests/post_spec.rb
+++ spec/requests/post_spec.rb
@@ -22,6 +22,20 @@ RSpec.describe 'Posts controller:', type: :request do
              expect(response.body).to include('This is my first post')
           end
       end
+
+      describe '/posts/show' do
+        before(:each) do
+          @user = User.create(name: 'Jugoslav', photo: 'https://i.imgur.com/5.jpg', bio: 'I am a test user.', posts_counter: 0)
+          @post = Post.create(author: @user, title: 'my post', text: 'this is my test post', likes_counter: 0,
+            comments_counter: 0)
+  
+          get post_path(@post)
+        end
+  
+        it('returns a success response') do
+          expect(response).to have_http_status(200)
+        end
     end
+  end
 end
   
