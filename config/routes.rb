6bc263b1a9cb2118ef1a71eb74b6afe1820d1b9f
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-12 19:16:13 +0200

    Add changes in files from folders assets, views, routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
 
+  # resources :users do
+  #   resources :posts
+  # end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-
+  # get '/users/:user_id/posts/:post_id
+  get   '/users/:user_id/posts/:id', to: 'posts#show' , as: 'user_post'
   root 'users#index'
 end
