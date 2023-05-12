a5884b463a61f36e8d9aa6ce779a20d1178ad242
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-12 19:21:26 +0200

    Fixing linters errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -79,4 +79,3 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   resources :likes
   resources :posts
   resources :users
-  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
   # get '/users/:user_id/posts/:post_id
-  get   '/users/:user_id/posts/:id', to: 'posts#show' , as: 'user_post'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
   root 'users#index'
 end
