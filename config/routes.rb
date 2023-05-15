8a47932d2d6ea414b64b84ca2c53ec1086e25100
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-15 19:37:28 +0200

    Add changes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,3 @@
-# Rails.application.routes.draw do
-#   resources :comments
-#   resources :likes
-#   resources :posts
-#   resources :users
-
-#   # resources :posts do
-#   #   resources :comments
-#   # end
-
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-#   # Defines the root path route ("/")
-#   # root "articles#index"
-#   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-#   # get '/users/:user_id/posts/:post_id
-#   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-#   get '/comments', to: 'comments#index' , as: 'comentari'
- 
-  
-#   root 'users#index'
-
-# end
-
 Rails.application.routes.draw do
   root 'users#index'
 
