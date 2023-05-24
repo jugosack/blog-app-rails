c9f817953720d2dc35aa9fe05f376a5f6c888758
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-24 21:37:38 +0200

    Add functionality for Hashed password, user register and login with user and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,9 +7,5 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
-  end
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
   end
 end
