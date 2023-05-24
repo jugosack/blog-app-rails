77be89a679a3755075b37ede083925acfb91ae5d
jugosack <jugoslav_ackoski@yahoo.com>
2023-05-24 17:56:46 +0200

    Add Devise gem, create views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_user
-    User.first
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio])
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
   end
 end
